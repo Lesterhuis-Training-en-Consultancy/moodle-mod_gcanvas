a4dfd5459d34c87010eddbd1e2a387e93675e3b1
Luuk Verhoeven <luuk@ldesign.nl>
2018-10-16 15:16:49 +0200

    Update styling & canvas js

--- amd/src/canvas.js
+++ amd/src/canvas.js
@@ -85,7 +85,7 @@ define(['jquery', 'core/notification'], function ($, notification) {
         /**
          * Toolbar actions.
          */
-        load_toolbar          : function () {
+        load_toolbar: function () {
 
             $('#toolbar .icon').click(function () {
 
@@ -100,7 +100,27 @@ define(['jquery', 'core/notification'], function ($, notification) {
                 let shape = "default_shape_" + elementtype.toLowerCase();
                 console.log("Search for shape: " + shape);
 
-                if (canvas_module.hasOwnProperty(shape)) {
+                if (elementtype === 'Arrow') {
+                    fabric.loadSVGFromURL('pix/arrow.svg', function (objects, options) {
+
+                        var shape = fabric.util.groupSVGElements(objects, options);
+                        canvas.add(shape.scale(0.1));
+                        shape.set({
+                            left: 200,
+                            top : 100
+                        }).setCoords();
+                        canvas.renderAll();
+
+                        canvas.forEachObject(function (obj) {
+                            var setCoords = obj.setCoords.bind(obj);
+                            obj.on({
+                                moving  : setCoords,
+                                scaling : setCoords,
+                                rotating: setCoords
+                            });
+                        })
+                    });
+                } else if (canvas_module.hasOwnProperty(shape)) {
                     console.log("Shape found");
 
                     let el = new fabric[elementtype](canvas_module[shape]);
@@ -112,6 +132,33 @@ define(['jquery', 'core/notification'], function ($, notification) {
                 }
 
                 canvas.requestRenderAll();
+            });
+        },
+
+        /**
+         * Don't allow going out of canvas.
+         */
+        prevent_moving_out_of_canvas: function () {
+            canvas.on('object:moving', function (e) {
+                var obj = e.target;
+                // if object is too big ignore
+                if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
+                    return;
+                }
+                obj.setCoords();
+
+                if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
+                    obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
+                    obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
+                }
+
+                if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height ||
+                    obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
+                    obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-
+                        obj.getBoundingRect().top);
+                    obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-
+                        obj.getBoundingRect().left);
+                }
             });
         },
 
@@ -135,7 +182,55 @@ define(['jquery', 'core/notification'], function ($, notification) {
                 'object:rotating': this.onchange,
             });
 
+            this.prevent_moving_out_of_canvas();
+
             this.load_toolbar();
+
+            this.add_horizontal_ruler();
+        },
+
+        /**
+         * Horizontal ruler.
+         */
+        add_horizontal_ruler: function () {
+            let ruler = new fabric.Rect({
+                width : this.canvas_width,
+                height: 1,
+                left  : 0,
+                top   : this.canvas_height / 2,
+                angle : 0,
+                fill  : '#5c5c5c'
+            });
+
+            ruler.flipY = false;
+            ruler.lockMovementX = true;
+            ruler.lockScalingX = true;
+            ruler.lockScalingY = true;
+            ruler.lockUniScaling = true;
+            ruler.lockRotation = true;
+
+            canvas.add(ruler);
+            canvas.requestRenderAll();
+
+            // Keyboard arrows move ruler.
+            $(document).keydown(function(e) {
+                switch(e.which) {
+
+                    case 38: // Up arrow.
+                        ruler.top =  ruler.top - 10;
+                        canvas.requestRenderAll();
+                        break;
+
+                    case 40: // Down arrow.
+                        ruler.top =  ruler.top + 10;
+                        canvas.requestRenderAll();
+
+                        break;
+
+                    default: return; // exit this handler for other keys
+                }
+                e.preventDefault(); // prevent the default action (scroll / move caret)
+            });
         },
 
         /**
@@ -143,14 +238,14 @@ define(['jquery', 'core/notification'], function ($, notification) {
          * @param options
          */
         onchange: function (options) {
-            options.target.setCoords();
-
-            canvas.forEachObject(function (obj) {
-                if (obj === options.target) {
-                    return;
-                }
-                obj.set('opacity', options.target.intersectsWithObject(obj) ? 0.5 : 1);
-            });
+            // options.target.setCoords();
+            //
+            // canvas.forEachObject(function (obj) {
+            //     if (obj === options.target) {
+            //         return;
+            //     }
+            //     obj.set('opacity', options.target.intersectsWithObject(obj) ? 0.5 : 1);
+            // });
         }
     };
 
