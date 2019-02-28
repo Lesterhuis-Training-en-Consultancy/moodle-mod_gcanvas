0cff3e89b51a9bb66a7a84d627c4045b65286274
Luuk Verhoeven <luuk@ldesign.nl>
2019-02-28 15:18:08 +0100

    Prefix css always with a plugin identifier
    https://docs.moodle.org/dev/Plugin_contribution_checklist#CSS_styles

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gcanvas';
 $plugin->release = '1.0.0';
-$plugin->version = 2019022700;
+$plugin->version = 2019022800;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_ALPHA;
