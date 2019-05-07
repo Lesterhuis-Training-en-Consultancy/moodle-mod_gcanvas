66bdc78fef222ea765236170f07688a7cfd4e15c
Luuk Verhoeven <luuk@ldesign.nl>
2019-05-07 14:16:04 +0200

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gcanvas';
-$plugin->release = '1.0.0';
-$plugin->version = 2019030400;
+$plugin->release = '1.0.1';
+$plugin->version = 2019050700;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_ALPHA;
