256670ee65c6c15990f4e41002cd8032635ebd0d
LT&C <gemma@ltnc.nl>
2022-08-15 10:17:00 +0200

    Update version.php
    version aangepast

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gcanvas';
-$plugin->release = '3.10.1';
+$plugin->release = '4.00.1';
 $plugin->version = 2022080800;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
